7c6e037db368a026bfb9c5cbbeac773d5115ec5f
Steven Cao <caohc2017@gmail.com>
2020-12-06 12:21:13 -0500

    Improve scaling method

--- src/main/java/com/ultimateboomer/textweaks/util/NativeImageUtil.java
+++ src/main/java/com/ultimateboomer/textweaks/util/NativeImageUtil.java
@@ -1,22 +1,25 @@
 package com.ultimateboomer.textweaks.util;
 
-import com.ultimateboomer.textweaks.TexTweaks;
-
 import net.minecraft.client.texture.NativeImage;
 
+@SuppressWarnings("unused")
 public class NativeImageUtil {
 	/**
-	 * Upscale image recursively
+	 * Upscale image
 	 * 
 	 * @param image input
-	 * @param scale times to run scaling (2x each run)
+	 * @param power times to run scaling (2x each run)
 	 * @return upscaled image
 	 */
-	public static NativeImage upscaleImage(NativeImage image, int scale) {
-		if (scale < 1) {
+	public static NativeImage upscaleImage(NativeImage image, int power) {
+		if (power < 1) {
 			return image;
 		}
 		
+		return upscaleImageFast(image, power);
+	}
+	
+	private static NativeImage upscaleImageRecursive(NativeImage image, int power) {
 		NativeImage newImage = new NativeImage(image.getFormat(), image.getWidth() * 2, image.getHeight() * 2, true);
 		for (int x = 0; x < image.getWidth(); ++x) {
 			for (int y = 0; y < image.getHeight(); ++y) {
@@ -24,17 +27,27 @@ public class NativeImageUtil {
 				newImage.setPixelColor(x * 2, y * 2, pixelColor);
 				newImage.setPixelColor(x * 2 + 1, y * 2, pixelColor);
 				newImage.setPixelColor(x * 2, y * 2 + 1, pixelColor);
-				newImage.setPixelColor(x * 2 + 1, y * 2 + 1, TexTweaks.config.debugFractal ? 0xFF000000 : pixelColor);
+				newImage.setPixelColor(x * 2 + 1, y * 2 + 1, pixelColor);
 			}
 		}
-		//MipmapTweaks.LOGGER.info("Upscaled texture " + scale);
-		
 		image.close();
-		if (scale == 1) {
+		if (power == 1) {
 			return newImage;
 		} else {
-			return upscaleImage(newImage, scale - 1);
+			return upscaleImageRecursive(newImage, power - 1);
 		}
-		
+
+	}
+	
+	private static NativeImage upscaleImageFast(NativeImage image, int power) {
+		int scale = 1 << power;
+		NativeImage newImage = new NativeImage(image.getFormat(), image.getWidth() * scale, image.getHeight() * scale, true);
+		for (int x = 0; x < image.getWidth(); ++x) {
+			for (int y = 0; y < image.getHeight(); ++y) {
+				newImage.fillRect(x * scale, y * scale, scale, scale, image.getPixelColor(x, y));
+			}
+		}
+		image.close();
+		return newImage;
 	}
 }
