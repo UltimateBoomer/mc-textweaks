4d906124b1bd8dc1fee9c275f3b5937add7ba78e
Steven Cao <caohc2017@gmail.com>
2021-01-25 17:09:58 -0500

    Rename TextureStitcherHolderMixin

--- src/main/java/io/github/ultimateboomer/textweaks/mixin/TextureStitcherMixin.java
+++ src/main/java/io/github/ultimateboomer/textweaks/mixin/TextureStitcherMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TextureStitcher.class)
-public class TextureStitcherHolderMixin {
+public class TextureStitcherMixin {
     /**
      * Reduce atlas size if mipmap level is set higher than necessary
      */
