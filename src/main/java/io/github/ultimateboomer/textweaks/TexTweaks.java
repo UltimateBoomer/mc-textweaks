474c73a95593826b10695f505cf7f728641a5381
Steven Cao <caohc2017@gmail.com>
2021-01-25 22:31:48 -0500

    Make texture info keybind unbound by default, add lang for texture info keybind, change texture info colors

--- src/main/java/io/github/ultimateboomer/textweaks/TexTweaks.java
+++ src/main/java/io/github/ultimateboomer/textweaks/TexTweaks.java
@@ -1,5 +1,6 @@
 package io.github.ultimateboomer.textweaks;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.ultimateboomer.textweaks.config.TexTweaksConfig;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
@@ -13,7 +14,6 @@ import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.LiteralText;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.lwjgl.glfw.GLFW;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -36,12 +36,9 @@ public class TexTweaks implements ClientModInitializer {
 		AutoConfig.register(TexTweaksConfig.class, GsonConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(TexTweaksConfig.class).getConfig();
 
-		// Register singleplayer command
-		//CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> TexTweaksCommand.register(dispatcher)));
-
 		// Register keybindings
 		keyInfo = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.textweaks.info",
-				InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J, "category.textweaks.main"));
+				InputUtil.Type.KEYSYM, -1, "category.textweaks"));
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (keyInfo.wasPressed()) {
 				if (client.player != null) {
@@ -51,11 +48,14 @@ public class TexTweaks implements ClientModInitializer {
 		});
 	}
 
+	/**
+	 * Show texture info
+	 */
 	public static void displayInfo(MinecraftClient mc) {
 		mc.player.sendMessage(new LiteralText(""), false);
 		mc.player.sendMessage(new LiteralText("\u00a7l[Texture Info]\u00a7r"), false);
 		mc.player.sendMessage(new LiteralText(
-				String.format("Max Atlas Size: \u00a7c%s\u00a7r", RenderSystem.maxSupportedTextureSize())), false);
+				String.format("Max Atlas Size: \u00a7a%s\u00a7r", RenderSystem.maxSupportedTextureSize())), false);
 
 		dataMap.forEach((id, data) -> {
 			mc.player.sendMessage(new LiteralText(
